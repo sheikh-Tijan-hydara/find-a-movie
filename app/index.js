de7f6e71096ff9e3049c2509ca98102b5333eef1
tijan <tijan@writemedia.co.uk>
2023-08-27 21:16:20 +0000

    ,,

--- app/index.js
+++ app/index.js
@@ -7,9 +7,9 @@ export default function App() {
     <SafeAreaView className="flex-1 items-center justify-center "> 
       <Header/>
       <ScrollView className=" w-full h-full bg-gray-100 mt-16 px-8 py-2">
-      <MovieTypes type="Action Movies" />
-      <MovieTypes type="Animi Movies" />
-      <MovieTypes type="Fiction Movies" />
+        <MovieTypes type="Action Movies" />
+        <MovieTypes type="Animi Movies" />
+        <MovieTypes type="Fiction Movies" />
       </ScrollView>
      
       <Footer activeTab="home"/>
